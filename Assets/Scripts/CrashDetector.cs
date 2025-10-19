90497ee1ca9f556b5bbb240ab872e612f2f80d52
Ventri051 <ventri051@morrisville.edu>
2025-10-19 19:47:12 -0400

    Added particle effects as per video lecture

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem crashParticle;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerIndex)
         {
+            crashParticle.Play();
             Invoke("ReloadScene", delay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem finishParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
         if (collision.gameObject.layer == layerIndex)
         {
+            finishParticles.Play();
             Invoke("ReloadScene", delay);
         }
     }
