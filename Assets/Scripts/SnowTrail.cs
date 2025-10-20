627ce21c3d42a235720f0fd486fd9b5ee6ea2421
Ventri051 <ventri051@morrisville.edu>
2025-10-19 22:40:38 -0400

    Added OnCollisionExit2D to detect collision exits.

---
+++ Assets/Scripts/SnowTrail.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SnowTrail : MonoBehaviour
+{
+    [SerializeField] ParticleSystem snowTrail;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor");
+        if (collision.gameObject.layer == layerIndex)
+        {
+            snowTrail.Play();
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        snowTrail.Stop();
+    }
+
+
+
+
+
+}
+
